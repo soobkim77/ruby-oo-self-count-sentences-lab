9cf62f6a633be36451e9ac415afed55e1c61a9dd
Soo-Beom <soobeomkim77@gmail.com>
2021-04-07 11:10:48 -0700

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else 
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else 
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    counter = 0
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
